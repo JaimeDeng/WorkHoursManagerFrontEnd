8f2ff960b7530eb3c77eac91058fad26ba6d3708
Chihli0802 <annannwhat@gmail.com>
2023-05-25 11:58:49 +0800

    update

--- src/components/StstemEditInfo.vue
+++ src/components/StstemEditInfo.vue
@@ -199,7 +199,7 @@ export default (await import('vue')).defineComponent({
                 if (this.langValue === 'ch') {
                     this.editMessage = "資料尚未進行任何修改";
                 } else if (this.langValue === 'en') {
-                    this.editMessage = "No modifications have been made to the data yet.";
+                    this.editMessage = "The date is not be edited.";
                 } else if (this.langValue === 'jp') {
                     this.editMessage = "データはまだ変更されていません。";
                 }
@@ -368,7 +368,7 @@ export default (await import('vue')).defineComponent({
                 this.checkPopupData.content = "您即將刪除此人員資訊 , 刪除後無法復原";
             } else if (this.langValue === 'en') {
                 this.checkPopupData.title = "Warning";
-                this.checkPopupData.content = "You are about to delete this personnel information, and it cannot be recovered once deleted.";
+                this.checkPopupData.content = "Do you want to delete data? you will can not return it.";
             } else if (this.langValue === 'jp') {
                 this.checkPopupData.title = "注意";
                 this.checkPopupData.content = "削除後は元に戻すことはできません。"
